deed81198712dc8a0a761befcc2198544c91fe90
Белявцев Артём <Arsainos@gmail.com>
2021-06-18 02:56:51 +0300

    add Example 2

---
+++ Camunda_Lesson2_Example2/Handlers/AskTImHandler.cs
@@ -0,0 +1,26 @@
+﻿using Camunda.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Camunda_Lesson2_Example2.Handlers
+{
+    [HandlerTopics("AskTim", LockDuration = 10000)]
+    public class AskTImHandler : IExternalTaskHandler
+    {
+        public async Task<IExecutionResult> HandleAsync(ExternalTask externalTask, CancellationToken cancellationToken)
+        {
+            await Task.Delay(5000);
+
+            return new CompleteResult
+            {
+                Variables = new Dictionary<string, Variable>
+                {
+                    ["Answer"] = Variable.String("Try to ask Tom")
+                }
+            };
+        }
+    }
+}
