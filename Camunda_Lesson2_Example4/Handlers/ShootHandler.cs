5844373f9bd2747197a26c770c6642717c83a443
Белявцев Артём <Arsainos@gmail.com>
2021-06-18 05:18:33 +0300

    add Example4

---
+++ Camunda_Lesson2_Example4/Handlers/ShootHandler.cs
@@ -0,0 +1,26 @@
+﻿using Camunda.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Camunda_Lesson2_Example4.Handlers
+{
+    [HandlerTopics("Shoot", LockDuration = 10000)]
+    public class ShootHandler : IExternalTaskHandler
+    {
+        public async Task<IExecutionResult> HandleAsync(ExternalTask externalTask, CancellationToken cancellationToken)
+        {
+            var shooted = Convert.ToBoolean(new Random().Next(0, 2));
+
+            return new CompleteResult
+            {
+                Variables = new Dictionary<string, Variable>
+                {
+                    ["Shooted"] = Variable.Boolean(shooted)
+                }
+            };
+        }
+    }
+}
---
+++ Camunda_Lesson2_Example4/Startup.cs
@@ -0,0 +1,59 @@
+using Camunda.Worker;
+using Camunda.Worker.Client;
+using Camunda_Lesson2_Example4.Handlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Camunda_Lesson2_Example4
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Конфигурируем подключенеие к Camunda
+            services.AddExternalTaskClient()
+                .ConfigureHttpClient((provider, client) =>
+                {
+                    client.BaseAddress = new Uri("http://localhost:8080/engine-rest"); // Регестрируем подключение к Camunda Engine
+                });
+
+            // Конфигурируем обработчик
+            services.AddCamundaWorker("ShootWorker")
+                .AddHandler<ShootHandler>() // указываем Handler для обработки запроса
+                .ConfigurePipeline(pipeline =>
+                {
+                    pipeline.Use(next => async context =>
+                    {
+                        var logger = context.ServiceProvider.GetRequiredService<ILogger<Startup>>(); // Использовать систему логирования 
+                        logger.LogInformation("Начала обработки таска {id}", context.Task.Id); // Записать в лог информацию
+                        await next(context); // Ожидание следующего запроса
+                        logger.LogInformation("Начала обработки таска {id}", context.Task.Id); // Записать в лог информацию
+                    });
+                });
+
+            services.AddHealthChecks(); // Добавить сервис работоспособности
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
