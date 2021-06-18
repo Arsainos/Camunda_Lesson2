2d3dea5bbba3f446d2f82cee4d64ba588d450d4e
Белявцев Артём <Arsainos@gmail.com>
2021-06-18 05:32:19 +0300

    add Example 5

---
+++ Camunda_Lesson2_Example5/Handlers/CarDriveHandler.cs
@@ -0,0 +1,18 @@
+﻿using Camunda.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Camunda_Lesson2_Example5.Handlers
+{
+    [HandlerTopics("DriveCar", LockDuration = 10000)]
+    public class CarDriveHandler : IExternalTaskHandler
+    {
+        public async Task<IExecutionResult> HandleAsync(ExternalTask externalTask, CancellationToken cancellationToken)
+        {
+            return new BpmnErrorResult("CAR_ERROR", "Engine is not working");
+        }
+    }
+}
---
+++ Camunda_Lesson2_Example5/Startup.cs
@@ -0,0 +1,64 @@
+using Camunda.Worker;
+using Camunda.Worker.Client;
+using Camunda_Lesson2_Example5.Handlers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Camunda_Lesson2_Example5
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Конфигурируем подключенеие к Camunda
+            services.AddExternalTaskClient()
+                .ConfigureHttpClient((provider, client) =>
+                {
+                    client.BaseAddress = new Uri("http://localhost:8080/engine-rest"); // Регестрируем подключение к Camunda Engine
+                });
+
+            // Конфигурируем обработчик
+            services.AddCamundaWorker("DriveCarWorker")
+                .AddHandler<CarDriveHandler>() // указываем Handler для обработки запроса
+                .ConfigurePipeline(pipeline =>
+                {
+                    pipeline.Use(next => async context =>
+                    {
+                        var logger = context.ServiceProvider.GetRequiredService<ILogger<Startup>>(); // Использовать систему логирования 
+                        logger.LogInformation("Начала обработки таска {id}", context.Task.Id); // Записать в лог информацию
+                        await next(context); // Ожидание следующего запроса
+                        logger.LogInformation("Начала обработки таска {id}", context.Task.Id); // Записать в лог информацию
+                    });
+                });
+
+            services.AddHealthChecks(); // Добавить сервис работоспособности
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHealthChecks("/health");
+        }
+    }
+}
